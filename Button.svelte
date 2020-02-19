9c0ddd59aea09c6fa49927d2a7e432456216adc6
MaciekTalaska <maciek.talaska@gmail.com>
2020-02-19 20:29:18 +0100

    ability to change separator & password length

--- Button.svelte
+++ Button.svelte
@@ -3,7 +3,7 @@
   import rollDices from './dice.js';
 
 	var diceCount = 4;
-	var passwordLength = 4;
+	let passwordLength = 4;
 	var words =  getWordsMap();
 
 	let password = "";
@@ -30,13 +30,36 @@
 	  border: none;
 	  padding: 8px 12px;
 	  border-radius: 2px;
+	  max-width: 60%;
+	}
+	div {
+		padding-bottom: 10px;
+	}
+	.container {
+		width: 600px;
+		border: 1px solid #ffffff;
+		display: inline-grid;
+	}
+	.column {
+		display: inline-block;
+		width: 50%;
 	}
 </style>
 
-<div>
-	<button on:click={generatePassword}>
-		Generate password
-	</button>
+<div class="container">
+	<div >
+		<label class="column">separator:</label>
+		<input class="column" bind:value={separator} />
+	</div>
+	<div>
+		<label class="column">password length:</label>
+		<input class="column" type="number" bind:value={passwordLength} min=4 max=10/>
+	</div>
+	<div>
+		<button on:click={generatePassword}>
+			Generate password
+		</button>
+	</div>
 	<p>generated password:</p>
 	<p>{password}</p>
 </div>
