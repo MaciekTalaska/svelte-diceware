3723815cb36a01ba1bae1d4169459fefacdf1cf7
MaciekTalaska <maciek.talaska@gmail.com>
2021-02-03 17:23:49 +0100

    remove extra margin-top

--- src/App.svelte
+++ src/App.svelte
@@ -13,7 +13,7 @@
   <h1>Diceware password generator in Svelte</h1>
   <Diceware />
   <small>
-    <p style="margin-top: 60px;">
+    <p>
       this project on github:
       <a href="https://github.com/MaciekTalaska/svelte-diceware">
         svelte-diceware
